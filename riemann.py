7d0fcd7f65a24b3d0f8559308e6d0c318a823945
Kaizhe <wkz9528@gmail.com>
2017-09-06 13:04:29 -0400

    Implyment right biased riemann

--- riemann.py
+++ riemann.py
@@ -1,15 +1,26 @@
 # This is a python file! The '#' character indicates that the following line is a comment.
 
+from __future__ import division
+import numpy as np
 # The following is an example for how to define a function in Python
 # def tells the compiler that hello_world is the name of a function
 # this implementation of hello_world takes a string as an argument,
-# which has default value of the empty string. If the user calls 
+# which has default value of the empty string. If the user calls
 # hello_world() without an argument, then the compiler uses ''
 # as the default value of the argument.
 def hello_world(name=''):
     print "hello world!"
     print name
     return
-    
-    
+
+
 #Implement the Riemann Sum approximation for integrals.
+
+hello_world(name='Kaizhe')
+
+def riemann(a, b, N, f):
+    width = (b-a)/N
+    x_vals = np.arange(a + width, b + width, width)
+    f_vals = f(x_vals)
+    result = np.sum(width * f_vals)
+    return result
